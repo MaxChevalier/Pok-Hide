cfa9d6c8b66e4b91a7b7443eaa70707011fe5843
MaxChevalier <maxime.chevalier@ynov.com>
2023-05-12 14:16:54 +0200

    matchmaking 1.0

--- Assets/Scrpit/Server/ConnectionToServer.cs
+++ Assets/Scrpit/Server/ConnectionToServer.cs
@@ -19,6 +19,6 @@ public class ConnectionToServer : MonoBehaviourPunCallbacks
 
     public override void OnJoinedLobby()
     {
-        SceneManager.LoadScene("Lobby");
+        SceneManager.LoadScene("Host&Play");
     }
 }
